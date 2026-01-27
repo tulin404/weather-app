d9cebfa37768e40c0eaac90fae8e77f4775348fa
Tulio <221194504+tulin404@users.noreply.github.com>
2026-01-27 15:06:49 -0300

    Added more languages

--- api/translate.js
+++ api/translate.js
@@ -3,15 +3,7 @@ import { translate } from "@vitalets/google-translate-api";
 export default async function translateHandler(req, res) {
     try {
         const {text, lang} = req.body;
-
-        if (!Array.isArray(text) || text.length === 0) {
-            return res.status(400).json({ error: "Campo 'text' inválido ou vazio" });
-        }
-
-        if (!lang || typeof lang !== "string") {
-            return res.status(400).json({ error: "Campo 'lang' inválido" });
-        }
-
+        
         const translated = await Promise.all(
             text.map(async element => {
                 const obj = await translate(element, {to: lang})
